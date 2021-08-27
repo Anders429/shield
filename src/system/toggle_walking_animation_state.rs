917c82171d8efa6603ea1546d76f23cce8078606
Anders429 <anders.stefan.evensen@gmail.com>
2021-08-27 08:12:49 -0700

    Walking animation only while entity is walking.

--- src/system/toggle_walking_animation_state.rs
+++ src/system/toggle_walking_animation_state.rs
@@ -3,7 +3,7 @@ use itertools::izip;
 
 pub(crate) fn toggle_walking_animation_state<const ENTITY_COUNT: usize>(world: &mut World<ENTITY_COUNT>) -> Events {
     for (entity, walking_timer, walking_animation_state) in izip!(world.entities.iter(), world.components.walking_timers.iter_mut(), world.components.walking_animation_states.iter_mut()) {
-        if entity.has_walking_timer() && entity.has_walking_animation_state() {
+        if entity.is_walking() && entity.has_walking_timer() && entity.has_walking_animation_state() {
             if *walking_timer == 0 {
                 *walking_animation_state = match walking_animation_state {
                     components::WalkingAnimationState::StandingA => components::WalkingAnimationState::StepA,
